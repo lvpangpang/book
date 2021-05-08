55b61cb8477b2ab3b1e5139609b1f1be4ba84cfc
lvpangpang <574818075@qq.com>
2021-05-08 16:01:58 +0800

    sokect

--- docs/其他/websocket/app.js
+++ docs/其他/websocket/app.js
@@ -1,26 +1,26 @@
-var express = require('express');
+var express = require("express");
 var app = express();
-var server  = require('http').createServer(app);
-var io = require('socket.io')(server);
+var server = require("http").createServer(app);
+var io = require("socket.io")(server);
 var port = process.env.PORT || 3000;
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + "/public"));
 
-app.get('/', function (req, res) {
-    res.sendFile('index.html');
+app.get("/", function (req, res) {
+  res.sendFile("index.html");
 });
 
-io.on('connection', function (socket) {
-    console.log('a user connected');
-    // 收到客戶端的abc命令將內容发送cba命令给客户端
-    socket.on('abc', function (data) {
-        io.emit('cba',data);
-    });
-    socket.on('disconnect', function () {
-        console.log('a user left');
-    })
+io.on("connection", function (socket) {
+  console.log("a user connected");
+  // 收到客戶端的abc命令將內容发送cba命令给客户端
+  socket.on("abc", function (data) {
+    io.emit("cba", data);
+  });
+  socket.on("disconnect", function () {
+    console.log("a user left");
+  });
 });
 
 server.listen(port, function () {
-    console.log('server start on port : %d',port);
+  console.log("server start on port : %d", port);
 });
--- docs/其他/websocket/chat.js
+++ docs/其他/websocket/chat.js
@@ -1,62 +1,61 @@
 //引入依赖并创建实例
-var express = require('express');
+var express = require("express");
 var app = express();
-let http = require('http').Server(app);//引入HTTP模块并启动服务
-let io = require('socket.io')(http);//引入socket.io模块
+let http = require("http").Server(app); //引入HTTP模块并启动服务
+let io = require("socket.io")(http); //引入socket.io模块
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + "/public"));
 
 //监听端口
 const port = 3000;
-http.listen(port,()=>{
-    console.log(`Http is listening to port ${port}`);
-})
+http.listen(port, () => {
+  console.log(`Http is listening to port ${port}`);
+});
 
 //创建变量
-let users = {};//接收所有用户的对象{name1,name2,name3}
-let onlineUsers = [];//接收所有在线用户的数组[{name:,id:},{name:,id:}]or[name1,name2,name3]
+let users = {}; //接收所有用户的对象{name1,name2,name3}
+let onlineUsers = []; //接收所有在线用户的数组[{name:,id:},{name:,id:}]or[name1,name2,name3]
 
 //创建事件
-io.on('connection',function(socket){
-
+io.on("connection", function (socket) {
   // 监听发私有消息事件==private message
-  socket.on('private message',(from,to,msg)=>{
-    if(to in users){
-      users[to].emit('to'+to,{msg,from,to});
+  socket.on("private message", (from, to, msg) => {
+    if (to in users) {
+      users[to].emit("to" + to, { msg, from, to });
     }
   });
 
   // 监听新用户进入聊天室事件==new user
-  socket.on('new user',name=>{
+  socket.on("new user", (name) => {
     // 只有当该用户不在用户列表中才进行操作
-    if(!(name in users)){
-      users[name] = socket;//将用户私有的socket保存给用户
-      onlineUsers.push(name);//将用户保存到在线列表
+    if (!(name in users)) {
+      users[name] = socket; //将用户私有的socket保存给用户
+      onlineUsers.push(name); //将用户保存到在线列表
     }
-    io.emit('online users',onlineUsers);//告诉前台用户在线用户列表
+    io.emit("online users", onlineUsers); //告诉前台用户在线用户列表
   });
 
   // 监听用户退出连接==disconnect
-  socket.on('disconnect',()=>{
+  socket.on("disconnect", () => {
     let logoutUserName;
     // 遍历所有用户，判断用户是否正在聊天室
-    for(let obj in users){
+    for (let obj in users) {
       // 如果在聊天室，提示退出，并在users中移除
-      if(users[obj]==socket){
+      if (users[obj] == socket) {
         logoutUserName = obj;
         delete users[obj];
       }
     }
     // 遍历所有在线用户，判断退出的用户是否在线
-    for(let i in onlineUsers){
+    for (let i in onlineUsers) {
       // 如果该用户在线的话，移除用户
-      if(onlineUsers[i]==logoutUserName){
-        onlineUsers.splice(i,1);
+      if (onlineUsers[i] == logoutUserName) {
+        onlineUsers.splice(i, 1);
       }
     }
     // 告诉前台在线用户列表
-    io.emit('online users',onlineUsers);
+    io.emit("online users", onlineUsers);
     // 告诉所有用户该用户已离开聊天室
-    io.emit('user disconnected',logoutUserName);
-  })
-})
+    io.emit("user disconnected", logoutUserName);
+  });
+});
